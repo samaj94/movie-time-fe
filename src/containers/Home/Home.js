e5b16607af5cba925bcc14c1ecd8f093c827a882
Sam <samaj94@gmail.com>
2021-06-07 08:58:48 -0400

    Changed RR to 5 for bootstap, more tutorial

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Movie Time</h1>
+        <p className="text-muted">What do you think?</p>
+      </div>
+    </div>
+  );
+}
